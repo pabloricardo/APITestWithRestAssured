dd2b0771abbfbfa477e1ab36f5a9db35e5a509d2
Pablo <pablo.mg@hotmail.com>
2020-06-16 21:02:26 -0300

    add new test

--- src/main/java/examples/AdvancedValidationTest.java
+++ src/main/java/examples/AdvancedValidationTest.java
@@ -1,7 +1,12 @@
 package examples;
 
+
+import io.restassured.path.json.JsonPath;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -10,7 +15,7 @@ public class AdvancedValidationTest {
 
     @Before
     public void printJson(){
-        System.out.println(get("https://restapi.wcaquino.me/users").asString());
+        //System.out.println(get("https://restapi.wcaquino.me/users").asString());
     }
 
     @Test
@@ -25,9 +30,21 @@ public class AdvancedValidationTest {
 
     @Test
     public void advancedValidationJsonPath(){
-        String temp = given()
+        ArrayList<String> names = given()
                 .when().get("https://restapi.wcaquino.me/users")
-                .path("findAll{it.age <= 25}").toString();
-        System.out.println(temp);
+                .then().statusCode(200)
+                .extract().path("findAll{it.age <= 25}.name");
+        System.out.println(names.get(0));
+    }
+
+    @Test
+    public void test(){
+        String resp = given()
+                .when().get("https://restapi.wcaquino.me/users")
+                .then().statusCode(200)
+                .extract().path("findAll{it.age <= 25}");
+        JsonPath jsonPath = new JsonPath(resp);
+        System.out.println(jsonPath.toString());
+
     }
 }
